18a86c2e475c4bd9194da32ef6b0b96408bfa944
SamyCode <sambea2103@gmail.com>
2024-01-13 17:17:24 -0500

    feat(app): create project scaffolding

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashbaordPage() {
+  return (
+    <div>DashbaordPage</div>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <p className='text-base'>Hello SaaS</p>
+      <Button>Click me</Button>
+    </main>
+  )
+}
