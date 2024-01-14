b522caad05f3fc4e7305d34abf97c0726afd7a97
SamyCode <sambea2103@gmail.com>
2024-01-14 14:58:56 -0500

    fix(hero): change padding on mobile view

--- app/(landing)/components/landing-hero.tsx
+++ app/(landing)/components/landing-hero.tsx
@@ -8,7 +8,7 @@ import TypeWriterComponent from 'typewriter-effect'
 export default function LandingHero() {
   const { isSignedIn } = useAuth()
   return (
-    <section className="space-y-5 p-36 text-center font-bold text-white">
+    <section className="space-y-5 p-8 md:p-36 text-center font-bold text-white">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
