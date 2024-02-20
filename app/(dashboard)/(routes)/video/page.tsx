8579bfba2feb947f6b7fdb1cb9a6881eb45b2b63
samudev <sambea2103@gmail.com>
2024-02-20 09:23:57 -0500

    feat: add page to dashboard routes

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -1,8 +1,8 @@
-export default function SettignsPage() {
+export default function VideoPage() {
   return (
     <section className="mb-4">
       <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">Settings</h2>
+        <h2 className="text-2xl md:text-4xl font-bold text-center">Video Generation</h2>
         <p className="text-muted-foreground text-sm md:text-lg font-light text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt distinctio eum.</p>
       </div>
       <div className="space-y-4 px-4 md:px-20 lg:px-32 pb-4">
